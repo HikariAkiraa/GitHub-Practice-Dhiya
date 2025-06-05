b060dafc1578a5d41140e40a3ba39d07453f1e91
Dhiya <muhammaddhiya2006@gmail.com>
2025-06-05 23:39:57 +0700

    Update hello.c display

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+
+void display() {
+    int a = 10;
+    printf("Halo!");
+    print("%d", a);
+    puts("MANTAO!");
+}
+
+
 int main() {
     int prime[] = {2, 3, 5, 7, 11};
     printf("Hellow Dunia World!");
